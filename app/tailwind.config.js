9ed1bc3108b1d087092650398ec7dea862562853
CharlieAlbert <charlieraph36@gmail.com>
2023-07-22 09:04:25 +0300

    Added two color palletes

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,html,css}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html,css}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     colors: {
+      background: "#292929",
       white: "#ffffff",
       secondary: "#dd455f",
       hover: "#d92f4c",
+      arrow: "#a7a7a7",
+      subtitle: "#e2e2e2",
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
