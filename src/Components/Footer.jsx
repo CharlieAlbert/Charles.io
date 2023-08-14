b62f24e502c780f50c3e038b34a52c17e1ddf58f
CharlieAlbert <charlieraph36@gmail.com>
2023-08-14 19:51:41 +0300

    Refactored folder structure

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+      <footer className="bg-inherit text-center dark:bg-neutral-700 lg:text-left">
         <div className="p-4 text-start text-neutral-700 dark:text-neutral-200">
           © 2023 albert.
         </div>
